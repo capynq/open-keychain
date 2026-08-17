5db3945d01a6d3f6a58cde440f6980e7a1bfad94
WilfredoN <wilfredochannel@gmail.com>
2026-08-17 14:30:15 +0300

    ci: allow generated dependency commit bodies

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,8 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Dependabot bodies contain long release-note URLs; keep header/subject
+    // validation while allowing those generated descriptions through CI.
+    'body-max-line-length': [0, 'always', 100],
+  },
 };
